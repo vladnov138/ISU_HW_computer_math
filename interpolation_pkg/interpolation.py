6f974477240d084d8388e5ba889b9affad78c6a6
vladnov138 <vladnov138@gmail.com>
2022-11-24 14:23:24 +0800

    Interpolation module fixed

--- interpolation_pkg/interpolation.py
+++ interpolation_pkg/interpolation.py
@@ -1,29 +1,26 @@
-import math
-import random
-from matplotlib import pyplot as plt
-
-import slae_by_inverse_matrix.slae_by_inverse_matrix as inv_m
 import matrix.matrix as mx
+import slae_by_inverse_matrix.slae_by_inverse_matrix as inv_m
+
 
 def split_matrix(coords_matrix):
     """Return y values as result and replace y values to 1 in coords_matrix"""
+    copy_coords_matrix = mx.get_copy(coords_matrix)
     res = []
-    for i in range(len(coords_matrix)):
-        res.append([coords_matrix[i][-1]])
-        coords_matrix[i][-1] = 1
-    return res
+    for i in range(len(copy_coords_matrix)):
+        res.append([copy_coords_matrix[i][-1]])
+        copy_coords_matrix[i][-1] = 1
+    return res, copy_coords_matrix
+
 
 def get_line_equation(coords_matrix):
     """Return line equation"""
-    copy_coords_matrix = [elem[:] for elem in coords_matrix]
-    res = split_matrix(copy_coords_matrix)
-    equation_matrix = inv_m.solve_slae(copy_coords_matrix, res)
+    res, new_coords_matrix = split_matrix(coords_matrix)
+    equation_matrix = inv_m.solve_slae(new_coords_matrix, res)
     return equation_matrix
 
 
 def interpolate(coords_matrix):
     """Return interpolate value"""
-    # print(coords_matrix)
     equation_matrix = get_line_equation(coords_matrix)
     x = min(coords_matrix[1][0], coords_matrix[0][0]) + \
         (max(coords_matrix[1][0], coords_matrix[0][0]) - min(coords_matrix[1][0], coords_matrix[0][0])) / 2
@@ -34,7 +31,7 @@ def extrapolate(coords_matrix, indent=3):
     """Return extrapolate value"""
     equation_matrix = get_line_equation(coords_matrix)
     x = min(coords_matrix[1][0], coords_matrix[0][0]) - indent
-    return [[x], [x + equation_matrix[-1][-1]]]
+    return [[x], [x * equation_matrix[0][0] + equation_matrix[-1][-1]]]
 
 
 def interpolate_piece_line(data_xy, indent=3):
@@ -46,47 +43,24 @@ def interpolate_piece_line(data_xy, indent=3):
         if i == 0:
             xy_matrix.append(mx.trans_matrix(extrapolate(a, indent=indent))[0])
         xy_matrix.append(mx.trans_matrix(interpolate(a))[0])
+
         if i == len(data_xy) - 2:
             xy_matrix.append(mx.trans_matrix(extrapolate(a, indent=-indent))[0])
     return xy_matrix
 
-def get_lagrange_polinomial(data_xy, x):
-    """"""
+
+def get_lagranzh_polinomial(data_xy, x):
+    """Return lagranzh polinomial value"""
     s = 0
     for i in range(len(data_xy)):
         s += data_xy[i][1] * get_product(data_xy, i, x)
     return s
 
+
 def get_product(data_xy, i, x):
     """Return a product of range"""
     prod = 1
     for j in range(len(data_xy)):
-        if j == i:
-            continue
-        prod *= (x - data_xy[j][0]) / (data_xy[i][0] - data_xy[j][0])
+        if j != i:
+            prod *= (x - data_xy[j][0]) / (data_xy[i][0] - data_xy[j][0])
     return prod
-
-def show_interpolate_extrapolate():
-    x = [2, 6]
-    y = [5, 9]
-    plt.plot(x, y)
-    intr_steps = interpolate([[2, 5], [6, 9]])
-    plt.step(intr_steps[0], intr_steps[1], "b-o")
-    extr_steps = extrapolate([[2, 5], [6, 9]], indent=1)
-    plt.step(extr_steps[0], extr_steps[1], "g-o")
-    plt.show()
-
-def show_interpolate_piece_line():
-    data_xy = [[1, 2], [3, 4], [3.5, 3], [6, 7]]
-    x = [1, 3, 3.5, 6]
-    y = [2, 4, 3, 7]
-    plt.plot(x, y)
-    # ans = [[-2, -1], [2, 3], [3.25, 3.5], [4.75, 5], [6.5, 3.9]]
-    steps = interpolate_piece_line(data_xy)
-    intr_steps = steps[1:-1]
-    plt.scatter(mx.trans_matrix(intr_steps)[0], mx.trans_matrix(intr_steps)[1], color="blue")
-    extr_steps = [steps[0], steps[-1]]
-    plt.scatter(mx.trans_matrix(extr_steps)[0], mx.trans_matrix(extr_steps)[1], color="orange")
-    plt.show()
-
-show_interpolate_piece_line()
--- interpolation_pkg/interpolation_test.py
+++ interpolation_pkg/interpolation_test.py
@@ -1,41 +1,47 @@
-from interpolation.interpolation import *
+from interpolation_pkg.interpolation import *
 
 EPS = 1e-10
+
 
 def test_split_matrix():
     coords_matrix = [[2, 5], [6, 9]]
     ans_coords_matrix = [[2, 1], [6, 1]]
     ans = [[5], [9]]
-    assert ans == split_matrix(coords_matrix)
-    assert coords_matrix == ans_coords_matrix
+    res, new_matrix = split_matrix(coords_matrix)
+    assert ans == res
+    assert new_matrix == ans_coords_matrix
+
 
 def test_get_line_equation():
     coords_matrix = [[2, 5], [6, 9]]
     ans = [[1], [3]]
     assert ans == get_line_equation(coords_matrix)
 
+
 def test_interpolate():
     coords_matrix = [[2, 5], [6, 9]]
     ans = [[4], [7]]
     assert ans == interpolate(coords_matrix)
+
 
 def test_extrapolate():
     coords_matrix = [[2, 5], [6, 9]]
     ans = [[1], [4]]
     assert ans == extrapolate(coords_matrix, indent=1)
 
+
 def test_interpolate_piece_line():
     data_xy = [[1, 2], [3, 4], [3.5, 3], [6, 7]]
-    ans = [[-2, -1], [2, 3], [3.25, 3.5], [4.75, 5], [6.5, 3.9]]
+    ans = [[-2, -1], [2, 3], [3.25, 3.5], [4.75, 5], [6.5, 7.8]]
     res = interpolate_piece_line(data_xy)
-    for i in range(len(res)):
-        for j in range(len(res[0])):
-            assert abs(res[i][j] - ans[i][j]) <= EPS
+    assert mx.equal_matrix_eps(ans, res, EPS)
 
-def test_get_lagrange_polinomial():
+
+def test_get_lagranzh_polinomial():
     data_xy = [[1, 2], [3, 4], [3.5, 3], [6, 7]]
     for i in range(len(data_xy)):
-        assert get_lagrange_polinomial(data_xy, data_xy[i][0]) == data_xy[i][1]
+        assert get_lagranzh_polinomial(data_xy, data_xy[i][0]) == data_xy[i][1]
+
 
 def test_get_product():
     data_xy = [[1, 2], [3, 4], [3.5, 3], [6, 7]]
