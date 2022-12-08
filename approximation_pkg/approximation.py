c8127a3b308a500a494bd2d19c01a37ab672dfec
vladnov138 <vladnov138@gmail.com>
2022-12-08 15:22:12 +0800

    approximation.py, approximation_view.py updated

--- approximation_pkg/approximation.py
+++ approximation_pkg/approximation.py
@@ -11,39 +11,20 @@ def lsm(a, b):
     return result
 
 
-def apprx(data_xy, x_wave, power=1, func=''):
-    """Returns a Y matrix by approximation"""
-    data_xy = add_func(data_xy, func)
-    x_wave = mx.trans_matrix(x_wave)
-    data_xy = ap_power(data_xy, power)
-    b, a = intp.split_matrix(data_xy)
-    equation = lsm(a, b)
-    x = mx.hstack(x_wave, [[1] * len(x_wave)])
-    x = add_func(x, func)
-    x = ap_power(x, power)
-    y = mx.mul_matrix(x, equation)
-    return y
-
-
-def ap_power(data_xy, power):
-    """Returns a new matrix by raising x position in power and swapping it"""
+def make_func(x, power):
+    """Returns func values"""
+    func = [x, 1]
     for i in range(2, power + 1):
-        line = [[x ** i for x in mx.trans_matrix(data_xy)[-2]]]
-        data_xy = mx.hstack(data_xy, line)
-        for j in range(len(data_xy[0]) - 1, 0, -1):
-            data_xy = mx.change_column(data_xy, j, j - 1)
-    return data_xy
+        val = x ** i
+        func.insert(0, val)
+    return func
 
 
-def add_func(data_xy, func):
-    """Returns a new matrix by adding a new column with function value"""
-    if type(func).__name__ == 'str':
-        return data_xy
-    # import math
-    # if type(func).__name__ != type(math.cos).__name__:
-    #     raise ValueError("Invalid type of function")
-    line = [[func(x) for x in mx.trans_matrix(data_xy)[0]]]
-    data_xy = mx.hstack(data_xy, line)
-    for i in range(len(data_xy[0]) - 1, 0, -1):
-        data_xy = mx.change_column(data_xy, i, i - 1)
-    return data_xy
+def apprx(data_xy, x_wave, power=1, func=make_func):
+    """Returns a Y matrix by approximation"""
+    a = [func(row_xy[0], power) for row_xy in data_xy]
+    x_wave = [func(x[0], power) for x in x_wave]
+    b, _ = intp.split_matrix(data_xy)
+    equation = lsm(a, b)
+    y = mx.mul_matrix(x_wave, equation)
+    return y
--- approximation_pkg/approximation_view.py
+++ approximation_pkg/approximation_view.py
@@ -4,20 +4,20 @@ from approximation_pkg.approximation import *
 from interpolation_pkg.interpolation_view import get_x, get_y
 
 
-def show_apprx(data_xy, points_x=0, power=1, func='', indent=1):
+def show_apprx(data_xy, x_wave, power=1, indent=1):
     """Draw approximation by pyplot"""
     points_data_x = get_x(data_xy)
     points_data_y = get_y(data_xy)
     # Generate x values
-    x = [[i / 10 for i in range((min(get_x(data_xy)) - indent) * 10,
-                                (max(get_y(data_xy)) + indent) * 10 + 1)]]
-    y = apprx(data_xy, x, power=power, func=func)
-    plt.plot(x[0], mx.trans_matrix(y)[0], color="blue")
+    x = [[i / 10, 1] for i in range((min(get_x(data_xy)) - indent) * 10,
+                                    (max(get_y(data_xy)) + indent) * 10 + 1)]
+    y = apprx(data_xy, x, power=power)
+    plt.plot(get_x(x), get_y(y), color="blue")
     # Points from data_xy
     plt.scatter(points_data_x, points_data_y, color="blue")
-    # Points from points_x (function argument)
-    points_y = apprx(data_xy, points_x, power=power, func=func)
-    plt.scatter(points_x, points_y, color="orange")
+    # Points from x_wave
+    points_y = apprx(data_xy, x_wave, power=power)
+    plt.scatter(get_x(x_wave), points_y, color="orange")
     plt.axis([min(get_x(data_xy)) - indent, max(get_x(data_xy)) + indent,
               min(get_y(data_xy)) - indent, max(get_y(data_xy)) + indent])
     plt.title("Approximation")
@@ -27,10 +27,6 @@ def show_apprx(data_xy, points_x=0, power=1, func='', indent=1):
     plt.show()
 
 
-def gh(x):
-    return [x, 1]
-
-
 data_xy = [[1, 2], [3, 4], [3.5, 3], [6, 7]]
-x = [[1, 3, 5]]
-show_apprx(data_xy, points_x=x, func=gh, indent=10)
+x = [[1, 1], [3, 1], [5, 1]]
+show_apprx(data_xy, x, power=1, indent=10)
